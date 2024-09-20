85a9f8433dc87f5d31b8a6eb641134ab57f81e01
Nina Higson <nina.higson@ao.com>
2024-09-20 11:39:55 +0100

    Add project files.

---
+++ SumOfTwoNumbers/SumOfTwoNumbers.cs
@@ -0,0 +1,10 @@
+﻿public static class Kata
+{
+    public static int sumTwoSmallestNumbers(int[] numbers)
+    {
+        int[] numb = [1, 2, 3, 4];
+        var sortedNumbers= numbers.OrderBy(numb => numb).ToArray();
+
+        return sortedNumbers[0] + sortedNumbers[1];
+    }
+}
